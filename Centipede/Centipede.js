b393000857002ff1a8dcb0a4252907af184637aa
lucasbrendborg3 <45109943+lucasbrendborg3@users.noreply.github.com>
2020-04-22 17:30:40 +0200

    Med snake

--- Centipede/Centipede.js
+++ Centipede/Centipede.js
@@ -1,99 +1,56 @@
-
 var game = document.getElementById("canvas");
 var context = game.getContext("2d");
-var direction;
-var hastighed = 200;
-context.fillstyle = "black";
 var test = document.getElementById("test");
 var test2 = document.getElementById("test2");
-var cells = [{x: 0,y: 0}, {x: 0,y: 0}, {x: 0,y: 0}];
-var maxcells = 4;
-var appleLoc;
+var direction;
+var hastighed = 200;
+var intervalPlane;
+var intervalSnake;
+var plane = {
+  x: 350,
+  y: 490,
+  width: 10,
+  height: 10
+}
 var snake = {
-  x: 0,
-  y: 0,
-  width: 13,
-  height: 13,
+  x: 10,
+  y: 10,
+  width: 15,
+  height: 15
 }
-var interval = setInterval(snakeMove, hastighed);
-apple();
-
-function snakeMove() {
-  if (direction === "left") {
-    snake.x -= 15;
-  }
-  if (direction === "up") {
-    snake.y -= 15;
-  }
-  if (direction === "right") {
-    snake.x += 15;
-  }
-  if (direction === "down") {
-    snake.y += 15;
-  }
-
-  context.fillRect(snake.x+1, snake.y+1, snake.width, snake.height);
-    cells.push({
-    x: snake.x,
-    y: snake.y,
-    width: snake.width,
-    height: snake.height});
-    if (cells.length > maxcells) {
-      context.clearRect(cells[0].x, cells[0].y, cells[0].width+2, cells[0].height+2)
-      cells.shift();
-    }
-    checkApple();
-    checkTail();
-    test2.innerHTML = "Score: " + cells.length;
-}
-
-function apple() {
-  let x = (Math.floor(Math.random() * Math.floor(39))) * 15;
-  let y = (Math.floor(Math.random() * Math.floor(39))) * 15;
-  context.fillStyle = "red";
-  context.fillRect(x+1, y+1, 13, 13);
-  context.fillStyle = "green";
-  appleLoc = {
-    x: x,
-    y: y
-  }
-}
-
-function checkApple() {
-  if (appleLoc.x === cells[maxcells-1].x && appleLoc.y === cells[maxcells-1].y) {
-    maxcells++;
-    apple();
-  }
-}
-
-function checkTail() {
-  for (let i; i < maxcells; i++) {
-    for (let j; j < maxcells; j++) {
-    if (cells[i].x === cells[j].x && cells[i].y === cells[j].y) {
-      console.log("You dead!!");
-       }
-     }
-   }
-}
-
+var snakeCells = [];
+movePlane();
+snakeMove();
 
 document.onkeydown = function(e) {
     e = e || window.event;
     switch(e.which || e.keyCode) {
         case 37: // left
-        left();
+        test.innerHTML = "Left";
+        direction = "left";
+        movePlane();
         break;
 
         case 38: // up
-        up();
+        test.innerHTML = "Up";
+        direction = "up";
+        movePlane();
         break;
 
         case 39: // right
-        right();
+        test.innerHTML = "Right";
+        direction = "right";
+        movePlane();
         break;
 
         case 40: // down
-        down();
+        test.innerHTML = "Down";
+        direction = "down";
+        movePlane();
+        break;
+
+        case 32: // space
+        skyd();
         break;
 
         default: return;
@@ -101,22 +58,54 @@ document.onkeydown = function(e) {
     e.preventDefault();
 };
 
-function left() {
-  test.innerHTML = "Left";
-  direction = "left";
+function movePlane() {
+  context.fillStyle = "yellow";
+  context.fillRect(plane.x, plane.y, plane.width, plane.height);
+  if (direction === "left") {
+    context.clearRect(plane.x, plane.y, plane.width, plane.height);
+    plane.x -= 5;
+    context.fillRect(plane.x, plane.y, plane.width, plane.height);
+  }
+  if (direction === "up") {
+    context.clearRect(plane.x, plane.y, plane.width, plane.height);
+    plane.y -= 5;
+    context.fillRect(plane.x, plane.y, plane.width, plane.height);
+  }
+  if (direction === "right") {
+    context.clearRect(plane.x, plane.y, plane.width, plane.height);
+    plane.x += 5;
+    context.fillRect(plane.x, plane.y, plane.width, plane.height);
+  }
+  if (direction === "down") {
+    context.clearRect(plane.x, plane.y, plane.width, plane.height);
+    plane.y += 5;
+    context.fillRect(plane.x, plane.y, plane.width, plane.height);
+  }
 }
 
-function up() {
-  test.innerHTML = "Up";
-  direction = "up";
+function skyd() {
+  let x = plane.x+4;
+  let y = plane.y-11;
+  intervalPlane = setInterval(function() {
+    context.fillStyle = "red";
+    context.clearRect(x, y--, 2, 10.5);
+    context.fillRect(x, y--, 2, 10);
+  }, 3);
 }
 
-function right() {
-  test.innerHTML = "Right";
-  direction = "right";
-}
-
-function down() {
-  test.innerHTML = "Down";
-  direction = "down";
+function snakeMove() {
+  intervalSnake = setInterval(function() {
+    snake.x += 15;
+    context.fillStyle = "green";
+    context.fillRect(snake.x+1, snake.y+1, snake.width, snake.height);
+      snakeCells.push({
+      x: snake.x,
+      y: snake.y,
+      width: snake.width,
+      height: snake.height});
+      if (snakeCells.length > 8) {
+        context.clearRect(snakeCells[0].x, snakeCells[0].y, snakeCells[0].width+2, snakeCells[0].height+2)
+        snakeCells.shift();
+    }
+  }, 100)
 }
