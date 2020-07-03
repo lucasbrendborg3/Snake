4e6b31238fa879646a0103766841b8aa17b7ca99
lucasbrendborg3 <45109943+lucasbrendborg3@users.noreply.github.com>
2020-07-03 18:10:11 +0200

    calculator

--- Centipede/Centipede.js
+++ Centipede/Centipede.js
@@ -11,52 +11,59 @@ var plane = {
   width: 10,
   height: 10
 }
-var snake = {
+var moreSnakes = [{
   x: -17,
   y: -15,
   width: 13,
   height: 13,
-  move: snakeMove(),
   direction: "right"
+}]
+var skud = {
+  x: 0,
+  y: 0,
+  width: 2,
+  height: 10
 }
 var skudActive = false;
-var snakeCells = [];
+var moreSnakeCells = [];
+var snakeLength = 8;
 movePlane();
+snakeMove(moreSnakes[0],moreSnakeCells);
 
 document.onkeydown = function(e) {
-    e = e || window.event;
-    switch(e.which || e.keyCode) {
-        case 37: // left
-        test.innerHTML = "Left";
-        direction = "left";
-        movePlane();
-        break;
+  e = e || window.event;
+  switch(e.which || e.keyCode) {
+    case 37: // left
+    test.innerHTML = "Left";
+    direction = "left";
+    movePlane();
+    break;
 
-        case 38: // up
-        test.innerHTML = "Up";
-        direction = "up";
-        movePlane();
-        break;
+    case 38: // up
+    test.innerHTML = "Up";
+    direction = "up";
+    movePlane();
+    break;
 
-        case 39: // right
-        test.innerHTML = "Right";
-        direction = "right";
-        movePlane();
-        break;
+    case 39: // right
+    test.innerHTML = "Right";
+    direction = "right";
+    movePlane();
+    break;
 
-        case 40: // down
-        test.innerHTML = "Down";
-        direction = "down";
-        movePlane();
-        break;
+    case 40: // down
+    test.innerHTML = "Down";
+    direction = "down";
+    movePlane();
+    break;
 
-        case 32: // space
-        skyd();
-        break;
+    case 32: // space
+    skyd(moreSnakeCells);
+    break;
 
-        default: return;
-    }
-    e.preventDefault();
+    default: return;
+  }
+  e.preventDefault();
 };
 
 function movePlane() {
@@ -84,16 +91,30 @@ function movePlane() {
   }
 }
 
-function skyd() {
+function skyd(snakeCells) {
   if (skudActive === false) {
-    let x = plane.x+4;
-    let y = plane.y-11;
+    let x = plane.x + 4;
+    let y = plane.y - 11;
     let intervalSkud = setInterval(function() {
       context.fillStyle = "red";
       context.clearRect(x, y--, 2, 10.5);
+      skud.x = 0;
+      skud.y = 0;
       context.fillRect(x, y--, 2, 10);
+      skud.x = x;
+      skud.y = y;
+      for (let i = 0; i < snakeCells.length; i++) {
+        if (skud.y > snakeCells[0].y - 15 && skud.y < snakeCells[0].y) {
+          if (skud.x < snakeCells[i].x + 15 && skud.x > snakeCells[i].x) {
+            skud.y = -10;
+            y = -10;
+            split(i);
+            return;
+          }
+        }
+      }
       skudActive = true;
-      if (y < -15) {
+      if (y < -10) {
         skudActive = false;
         clearInterval(intervalSkud);
       }
@@ -101,18 +122,36 @@ function skyd() {
   }
 }
 
-function snakeMove() {
+function split(i) {
+  console.log(moreSnakeCells.slice(i, moreSnakeCells[0].length));
+  moreSnakeCells.push()
+  moreSnakeCells.pop(i);
+  snakeLength--;
+}
+
+function snakeMove(snake,snakeCells) {
   intervalSnake = setInterval(function() {
-if (snake.x === 508) {
-  snake.direction = "left";
-  snake.y += 15;
-}
-if (snake.x === -17) {
-  snake.direction = "right";
-  snake.y += 15;
-}
-      if (snake.direction === "right") {
-          snake.x += 15;
+    if (snake.x === 508) {
+      snake.direction = "left";
+      snake.y += 15;
+    }
+    if (snake.x === -17) {
+      snake.direction = "right";
+      snake.y += 15;
+    }
+    if (snake.direction === "right") {
+      snake.x += 15;
+      context.fillStyle = "green";
+      context.fillRect(snake.x+2, snake.y+2, snake.width, snake.height);
+      snakeCells.push({
+        x: snake.x,
+        y: snake.y,
+        width: snake.width,
+        height: snake.height});
+      }
+      if (snake.direction === "left") {
+        if (snake.direction === "left") {
+          snake.x -= 15;
           context.fillStyle = "green";
           context.fillRect(snake.x+2, snake.y+2, snake.width, snake.height);
           snakeCells.push({
@@ -120,24 +159,11 @@ if (snake.x === -17) {
             y: snake.y,
             width: snake.width,
             height: snake.height});
-                        console.log(snake.x);
-      }
-      if (snake.direction === "left") {
-          if (snake.direction === "left") {
-              snake.x -= 15;
-              context.fillStyle = "green";
-              context.fillRect(snake.x+2, snake.y+2, snake.width, snake.height);
-              snakeCells.push({
-                x: snake.x,
-                y: snake.y,
-                width: snake.width,
-                height: snake.height});
-                            console.log(snake.x);
+          }
+        }
+        if (snakeCells.length > snakeLength) {
+          context.clearRect(snakeCells[0].x, snakeCells[0].y, snakeCells[0].width+2, snakeCells[0].height+2)
+          snakeCells.shift();
+        }
+      }, 100)
     }
-  }
-    if (snakeCells.length > 8) {
-      context.clearRect(snakeCells[0].x, snakeCells[0].y, snakeCells[0].width+2, snakeCells[0].height+2)
-      snakeCells.shift();
-  }
-  }, 100)
-}
